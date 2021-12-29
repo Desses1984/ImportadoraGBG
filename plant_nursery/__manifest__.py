7a84eca28a057cbb60d1f8fd0ee7525cd99a84b3
Daniel Esses <danielesses@gmail.com>
2021-12-29 19:33:46 +0000

    Nuevo Cambio

---
+++ plant_nursery/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Plant Nursery',
+    'version': '0.1',
+    'category': 'Uncategorized',
+    'summary': "Plant and customers managment",
+    'depends': ['web'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    'demo': [
+        'demo/demo.xml'
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+            ],
+}
+
+   
---
+++ plant_nursery/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class Plants(models.Model):
+    _name = 'nursery.plant'
+
+
+     name = fields.Char("Plant Name")
+     price = fields.float()
+        
+class custumer(models.Model)
+    _name = 'nursery.customer'
+    
+    
+     name = fields.Char("Customer Name",required=True)
+     email = fields.Char(help="To Recieve the Newsletter")
